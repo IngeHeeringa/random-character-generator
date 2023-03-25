523b9d3824f8e548758634efe2d3bf24a839a63f
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-25 21:39:57 +0100

    Modularize getAgeRange function

--- js/generator.js
+++ js/generator.js
@@ -23,39 +23,7 @@ import {
 } from "./selectors.js";
 import checkGender from "./utils/checkGender.js";
 import checkLifeStage from "./utils/checkLifeStage.js";
-
-const getAgeRange = () => {
-  const lifeStage = checkLifeStage();
-  const ageRange = {
-    min: "",
-    max: "",
-  };
-
-  switch (lifeStage) {
-    case "child":
-      ageRange.min = 4;
-      ageRange.max = 13;
-      break;
-    case "teenager":
-      ageRange.min = 13;
-      ageRange.max = 19;
-      break;
-    case "youngadult":
-      ageRange.min = 19;
-      ageRange.max = 25;
-      break;
-    case "adult":
-      ageRange.min = 25;
-      ageRange.max = 66;
-      break;
-    case "senior":
-      ageRange.min = 66;
-      ageRange.max = 105;
-      break;
-  }
-
-  return ageRange;
-};
+import getAgeRange from "./utils/getAgeRange.js";
 
 const getRandomAge = () => {
   const ageRange = getAgeRange();
