aba60b4ca1a73665eb46b81bf9e2a4e7cdd86306
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-25 22:00:18 +0100

    Modularize getRandomSchoolExperience function

--- js/generator.js
+++ js/generator.js
@@ -1,5 +1,5 @@
 import { traits, dreams, advice } from "./database/personality.js";
-import { schoolExperience, jobs } from "./database/occupation.js";
+import { schoolExperience } from "./database/occupation.js";
 import { hobbies, interests } from "./database/pastimes.js";
 import { familyMembers, residence } from "./database/livingSituation.js";
 import {
@@ -22,51 +22,7 @@ import getRandomPersonalityType from "./utils/getRandomPersonalityType.js";
 import getRandomStarSign from "./utils/getRandomStarSign.js";
 import getRandomStudies from "./utils/getRandomStudies.js";
 import getRandomJob from "./utils/getRandomJob.js";
-
-const getRandomSchoolExperience = () => {
-  const birthYear = getBirthYear();
-  const classicAfterSchoolActivities =
-    schoolExperience.teenager.afterSchoolActivities.classic;
-  const modernAfterSchoolActivities =
-    schoolExperience.teenager.afterSchoolActivities.modern.concat(
-      schoolExperience.teenager.afterSchoolActivities.classic
-    );
-
-  const randomSchoolExperienceChild = {
-    subjects:
-      schoolExperience.child.subjects[
-        Math.floor(Math.random() * schoolExperience.child.subjects.length)
-      ],
-    experience:
-      schoolExperience.child.experience[
-        Math.floor(Math.random() * schoolExperience.child.experience.length)
-      ],
-  };
-
-  const randomSchoolExperienceTeenager = {
-    subjects:
-      schoolExperience.teenager.subjects[
-        Math.floor(Math.random() * schoolExperience.teenager.subjects.length)
-      ],
-    experience:
-      schoolExperience.teenager.experience[
-        Math.floor(Math.random() * schoolExperience.teenager.experience.length)
-      ],
-    afterSchoolActivities: "",
-  };
-  if (birthYear < 1970) {
-    randomSchoolExperienceTeenager.afterSchoolActivities =
-      classicAfterSchoolActivities[
-        Math.floor(Math.random() * classicAfterSchoolActivities.length)
-      ];
-  } else
-    randomSchoolExperienceTeenager.afterSchoolActivities =
-      modernAfterSchoolActivities[
-        Math.floor(Math.random() * modernAfterSchoolActivities.length)
-      ];
-
-  return { randomSchoolExperienceChild, randomSchoolExperienceTeenager };
-};
+import getRandomSchoolExperience from "./utils/getRandomSchoolExperience.js";
 
 const getRandomCharacterTraits = () => {
   const birthYear = getBirthYear();
