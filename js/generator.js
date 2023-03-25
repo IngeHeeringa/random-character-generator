98da4349d1ef541200fe3108b63b883a5178f2bb
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-25 21:50:41 +0100

    Modularize getRandomName function

--- js/generator.js
+++ js/generator.js
@@ -24,39 +24,7 @@ import checkGender from "./utils/checkGender.js";
 import checkLifeStage from "./utils/checkLifeStage.js";
 import getRandomAge from "./utils/getRandomAge.js";
 import getBirthYear from "./utils/getBirthYear.js";
-import getNameByDecade from "./utils/getNameByDecade.js";
-
-const getRandomName = () => {
-  const { gender } = checkGender();
-  const charNationality = countryInput.value;
-  let randomName;
-
-  switch (charNationality) {
-    case "es":
-      if (gender === "female") {
-        randomName = getNameByDecade(namesES.femaleNames);
-      } else if (gender === "male") {
-        randomName = getNameByDecade(namesES.maleNames);
-      }
-      break;
-    case "nl":
-      if (gender === "female") {
-        randomName = getNameByDecade(namesNL.femaleNames);
-      } else if (gender === "male") {
-        randomName = getNameByDecade(namesNL.maleNames);
-      }
-      break;
-    case "uk":
-      if (gender === "female") {
-        randomName = getNameByDecade(namesUK.femaleNames);
-      } else if (gender === "male") {
-        randomName = getNameByDecade(namesUK.maleNames);
-      }
-      break;
-  }
-
-  return randomName;
-};
+import getRandomName from "./utils/getRandomName.js";
 
 const getRandomSurname = () => {
   const charNationality = countryInput.value;
